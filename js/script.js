44178137ef56b8f0e079c29516c9b9db69cd1969
Pyae Thein <pyaethein246@gmail.com>
2022-06-16 21:33:12 +0630

    commit

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const noNotes = document.querySelector('.no-notes');
 // event listeners
 form.addEventListener('submit', addNote);
 noteList.addEventListener('click', deleteNote);
+// noteList.addEventListener('click', viewModal);
 
 // functions
 function addNote(e){
@@ -23,6 +24,18 @@ function addNote(e){
     noNotes.remove();
   }
 
+  // omit text
+  const MAX_BODY_LENGTH = 60;
+
+  if(textAreaInput.innerText.length > MAX_BODY_LENGTH){
+    textAreaInput.innerHTML = `
+    ${textAreaInput.innerText.substring(0, MAX_BODY_LENGTH)}
+    ${textAreaInput.innerText.length > MAX_BODY_LENGTH ? '...' : ''}
+    `;
+  }
+
+  console.log(textAreaInput.innerText.length);
+
   const noteDiv = document.createElement('div');
   noteDiv.classList.add('note');
 
@@ -30,7 +43,7 @@ function addNote(e){
   noteIndex.classList.add('note__index');
   
   // noteList.children.length = zero by default
-  console.log(noteList.children.length);
+  // console.log(noteList.children.length);
   const updatedNoteIndex = noteList.children.length + 1;
   noteIndex.innerText = `Note ${updatedNoteIndex}`;
 
@@ -50,9 +63,9 @@ function addNote(e){
   noteList.appendChild(noteDiv);
 
   if(noteList.children.length > 1){
-    noteList.style.gridTemplateColumns = '1fr 1fr';
+    noteList.style.gridTemplateColumns = '50% 50%';
   } else{
-    noteList.style.gridTemplateColumns = '1fr';
+    noteList.style.gridTemplateColumns = '100%';
   }
 }
 
@@ -60,11 +73,23 @@ function deleteNote(e){
 
   if(e.target.classList.contains('delete-btn')){
     e.target.parentElement.remove();
+
+    // if(noteList.children.length = 1){
+    //   noteList.appendChild(noNotes);
+    // }
   }
 
   // for(i = 0; i < noteList.children.length; i++){
-  //   console.log(noteList.children.length);
-  //   const updatedNoteIndex = noteList.children.length - i;
-  //   e.target.previousElementSibling.previousElementSibling.previousElementSibling.innerText = `Note ${updatedNoteIndex}`;
+  //   // console.log(noteList.children.length);
+  //   const updatedNoteIndex = noteList.children.length - 1;
+  //   // e.target.previousElementSibling.previousElementSibling.previousElementSibling.innerText = `Note ${updatedNoteIndex}`;
+  //   // console.log(updatedNoteIndex);
   // }
 }
+
+// function viewModal(e){
+
+//   if(e.target.classList.contains('view-btn')){
+
+//   }
+// }
